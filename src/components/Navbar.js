f7823cf630bfff4794f95eff8c115054b499ae83
vishnusnair5471 <vishnusnair5471@gmail.com>
2025-11-13 15:35:47 +0530

    hi bro

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
     <nav className="navbar">
       <h2 className="logo">MyPortfolio</h2>
       <ul>
-        <li className={location.pathname === "/" ? "active" : ""}><Link to="/">Home</Link></li>
+        <li className={location.pathname === "/" ? "active" : ""}><Link to="/">Home </Link></li>
         <li className={location.pathname === "/about" ? "active" : ""}><Link to="/about">About</Link></li>
         <li className={location.pathname === "/projects" ? "active" : ""}><Link to="/projects">Projects</Link></li>
         <li className={location.pathname === "/skills" ? "active" : ""}><Link to="/skills">Skills</Link></li>
